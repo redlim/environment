5124fbf73f2fcd081296f45ea36cdc68a96fe0a5
Will <wolmos@m4v.es>
2017-01-30 23:44:52 +0100

    modificacion de la forma en la que se guardan las fechas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const enviroment = require('../calculation/environment.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Enviroment' });
+});
+
+router.get('/data', function(req, res, next) {
+
+  enviroment.getData(req.query.date,function (err,data) {
+      if(!err)
+        res.send(data);
+      else
+        console.error(err);
+  })
 });
 
 module.exports = router;
