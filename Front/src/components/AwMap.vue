9383d61e4c38808a1371b2af2134aee33bc588a0
Will <wolmos@m4v.es>
2018-02-12 20:09:51 +0100

    adding the markers on the map

--- Front/src/components/AwMap.vue
+++ Front/src/components/AwMap.vue
@@ -1,42 +1,45 @@
 <template>
   <div class="hello">
-    <gmap-map
-      :center="center"
-      :zoom="9"
-      map-type-id="terrain"
-      style="width: 500px; height: 300px"
-    >
-      <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        :clickable="true"
-        :draggable="true"
-        @click="center=m.position"
-      ></gmap-marker>
-    </gmap-map>
+    <div class="google-map" :id="mapName"></div>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'AwMap',
     data () {
       return {
+        name:"miMapa",
+        mapName: this.name + "-map",
         madridData:[],
         msg: 'This is Enviroment!',
         center: {lat: 40.416775, lng: -3.703790},
-        markers: []
+        markers: [],
+        map:{}
       }
     },
     mounted(){
+      const element = document.getElementById(this.mapName);
+      this.bounds = new google.maps.LatLngBounds();
+      const options = {
+        center: new google.maps.LatLng(this.center.lat, this.center.lng)
+      };
+      this.map = new google.maps.Map(element, options);
       this.$http.get('http://localhost:5000/data').then(response => {
 
         // get body data
         this.madridData = response.body;
         this.madridData.forEach((d)=>{
           console.log(d.stationCoordinates[0]);
+          const position = new google.maps.LatLng(d.stationCoordinates[0], d.stationCoordinates[1]);
+          const marker = new google.maps.Marker({
+            position,
+            map: this.map
+          });
+          this.markers.push(marker);
           this.markers.push({position:{lat:parseFloat(d.stationCoordinates[0]),lng:parseFloat(d.stationCoordinates[1])}});
+          this.map.fitBounds(this.bounds.extend(position))
         });
 
       }, response => {
@@ -48,4 +51,10 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .google-map {
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+    background: gray;
+  }
 </style>
--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -3,17 +3,6 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import AwMap from '@/components/AwMap'
 import VueResource from 'vue-resource';
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyDCj1jfd3UZRLxXQoxY66bNoePod9XYk0A',
-    libraries: 'places', // This is required if you use the Autocomplete plugin
-    // OR: libraries: 'places,drawing'
-    // OR: libraries: 'places,drawing,visualization'
-    // (as you require)
-  }
-});
 
 Vue.use(VueResource);
 Vue.use(Router);
